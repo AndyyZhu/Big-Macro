f6845ef39d5f4d01cbb9e48b11e1aea17af4a4be
Kyle Zhou <51883176+Kyle-Zhou@users.noreply.github.com>
2023-12-29 17:17:44 -0500

    temp add icons to landing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -19,10 +19,21 @@ export default async function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       
-      <h1 className="pt-40 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Big Macro
+
+      <Image src="/Subway.png" width={60} height={60} alt="Subway" className="absolute w-14 h-14 left-40 top-20 transform hover:rotate-180 transition-transform duration-300 ease-in-out" />
+      <Image src="/Mcd.png" width={75} height={75} alt="Mcd" className="absolute w-20 h-20 left-20 top-60 transform rotate-[-45] transform hover:rotate-180 transition-transform duration-300 ease-in-out" />
+      <Image src="/Popeyes.png" width={75} height={75} alt="Popeyes" className="absolute w-14 h-14 right-40 top-60 transform rotate-45 transform hover:rotate-180 transition-transform duration-300 ease-in-out" />
+      <Image src="/Chipotle.png" width={75} height={75} alt="Chipotle" className="absolute w-18 h-18 right-20 top-20 transform hover:rotate-180 transition-transform duration-300 ease-in-out" />
+      <Image src="/Tims.png" width={75} height={75} alt="Tims" className="absolute w-12 h-12 right-10 top-80 transform hover:rotate-180 transition-transform duration-300 ease-in-out" />
+
+
+      <h1 className="pt-40 pb-8 mx-20 max-w-6xl bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-5xl font-bold tracking-tight md:text-7xl">
+        Uncover the Best Macronutrient-Rich Meals from Local Menus
       </h1>
-      
+      <h2 className="pb-8 mx-20 max-w-6xl text-center text-xl font-normal tracking-tight md:text-2xl">
+        Discover the quickest and easiest fast-food protein options, perfect for when you need to grab a bite on the go. Enjoy the convenience of fast food without sacrificing your macronutrient goals.
+      </h2>
+
       <Link
         href="https://vercel.com/templates/next.js/postgres-prisma"
         className="group mt-20 mb-10 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
