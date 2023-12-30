b753ed54e46e8245282f3108b99fece7819661e6
Andy Zhu <andyzhu200@gmail.com>
2023-12-30 00:39:30 -0500

    testing pulling all food items w protein from Tim Hortons

--- server/controllers/foodItems.ts
+++ server/controllers/foodItems.ts
@@ -31,6 +31,7 @@ foodRouter.get('/getMenuItems', async (req, res) => {
 foodRouter.get('/getNutritionalInfo', async (req, res) => {
   try {
     const nutrition = await prisma.nutritionalinfo.findMany()
+    getHighestProtein(['Tim Hortons'])
     res.json(nutrition)
   } catch (error) {
     console.error('Error fetching places:', error);
@@ -48,9 +49,62 @@ foodRouter.get('/calcNutrition', async (req, res) => {
   }
 });
 
-function getHighestProtein(restaurants : []) {
+async function getHighestProtein(restaurants: string[]): Promise<Array<{ restaurant: string, menuItem: string, proteinAmount: number }>> {
+  const result: Array<{ restaurant: string, menuItem: string, proteinAmount: number }> = [];
 
+  for (const restaurant of restaurants) {
+    console.log(restaurant)
+    try {
+      const restaurantData = await prisma.restauranttypes.findFirst({
+        where: { name: restaurant },
+        include: {
+          menuitems: {
+            include: {
+              nutritionalinfo: {
+                select: {
+                  protein_grams: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      console.log("Test", restaurantData)
+
+      // if (restaurantData) {
+      //   const menuItems = restaurantData.menuitems;
+      //   let maxProteinItem: { restaurant: string, menuItem: string, proteinAmount: number } | null = null;
+      //   let maxProteinAmount = 0;
+
+      //   for (const menuItem of menuItems) {
+      //     const proteinAmount = menuItem.nutritionalinfo.reduce(
+      //       (total, info) => total + info.protein_grams,
+      //       0
+      //     );
+
+      //     if (proteinAmount > maxProteinAmount) {
+      //       maxProteinAmount = proteinAmount;
+      //       maxProteinItem = {
+      //         restaurant,
+      //         menuItem: menuItem.name,
+      //         proteinAmount,
+      //       };
+      //     }
+      //   }
+
+      //   if (maxProteinItem) {
+      //     result.push(maxProteinItem);
+      //   }
+      // }
+    } catch (error) {
+      console.error(`Error processing restaurant ${restaurant}:`, error);
+    }
+  }
+
+  return result;
 }
+
 
 function getProteinCalRatio(restaurants : []) {
   
